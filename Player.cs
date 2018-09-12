307d68d60ffceecd18a663877e0c08609bbd629d
mekatama <tamatte@hotmail.com>
2018-09-12 23:29:25 +0900

    playerの旋回と弾発射

---
+++ Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	private float rayRange = 1000f;			//レイを飛ばす距離
+	private Vector3 targetPosition;			//移動する位置
+
+	public GameObject bulletObject = null;			//弾プレハブ
+	public Transform bulletStartPosition = null;	//弾の発射位置を取得する
+	public float timeOut = 0.4f;					//弾の連射間隔
+	private float timeElapsed = 0.0f;				//弾の連射間隔カウント用
+
+	void Start () {
+		targetPosition = transform.position;	//playerの位置
+	}
+	
+	void Update () {
+		//マウスクリックの位置に向く
+		//マウスのタッチとタッチしっぱなしを判定
+		if(Input.GetMouseButtonDown(0) || Input.GetMouseButton(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			//地面にFieldレイヤーを設定してタッチ位置を取得
+			if(Physics.Raycast(ray, out hit, rayRange, LayerMask.GetMask ("Field"))) {
+				targetPosition = hit.point;
+				targetPosition.y = 0.5f;			//y座標固定
+				transform.LookAt(targetPosition);	//向く
+			}
+		}
+
+		//弾の自動連射
+		timeElapsed += Time.deltaTime;
+        if(timeElapsed >= timeOut) {
+			//弾を生成する位置を指定する
+			Vector3 vecBulletPos	= bulletStartPosition.position;
+			//弾を生成する
+			Instantiate( bulletObject, vecBulletPos, transform.rotation);
+			timeElapsed = 0.0f;
+		}
+	}
+ }
