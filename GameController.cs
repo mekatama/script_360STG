bcf0a62d94b6bcaa6427c6c505c22de0e6abdae7
mekatama <tamatte@hotmail.com>
2018-09-14 19:17:35 +0900

    scoreを用意

---
+++ GameController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public int total_Score;		//score入れ
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
