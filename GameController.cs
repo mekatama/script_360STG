efe12c214fa80e3b02c2d8b8b471b4597b9af51f
mekatama <tamatte@hotmail.com>
2018-10-11 19:47:21 +0900

    制御用数値を設定

--- GameController.cs
+++ GameController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 	public int total_Score;		//score
 	public int attackPower;		//攻撃力
+	public int total_ItemNum;	//item数
+	public int killEnemyNum;	//撃墜enemy数
+	public bool isMuteki;		//無敵flag
 
 	void Start () {
 		
 	}
 	
 	void Update () {
-		
+//		Debug.Log("kill数" + killEnemyNum);
 	}
 }
