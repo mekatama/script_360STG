c6dc509d8830fdaebce245410ece3e524540660f
mekatama <tamatte@hotmail.com>
2018-10-17 19:34:56 +0900

    shot制御削除

--- Player.cs
+++ Player.cs
@@ -7,11 +7,6 @@ public class Player : MonoBehaviour {
 	private float rayRange = 1000f;			//レイを飛ばす距離
 	private Vector3 targetPosition;			//移動する位置
 
-	public GameObject bulletObject = null;			//弾プレハブ
-	public Transform bulletStartPosition = null;	//弾の発射位置を取得する
-	public float timeOut = 0.4f;					//弾の連射間隔
-	private float timeElapsed = 0.0f;				//弾の連射間隔カウント用
-
 	void Start () {
 		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
 		targetPosition = transform.position;	//playerの位置
@@ -29,16 +24,6 @@ public class Player : MonoBehaviour {
 				targetPosition.y = 0.5f;			//y座標固定
 				transform.LookAt(targetPosition);	//向く
 			}
-		}
-
-		//弾の自動連射
-		timeElapsed += Time.deltaTime;
-        if(timeElapsed >= timeOut) {
-			//弾を生成する位置を指定する
-			Vector3 vecBulletPos	= bulletStartPosition.position;
-			//弾を生成する
-			Instantiate( bulletObject, vecBulletPos, transform.rotation);
-			timeElapsed = 0.0f;
 		}
 	}
 
