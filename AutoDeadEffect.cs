1bafc726a94601601ab10438da5dbf70640f8e0b
mekatama <tamatte@hotmail.com>
2018-11-07 19:50:04 +0900

    effect自力削除

---
+++ AutoDeadEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDeadEffect : MonoBehaviour {
+	ParticleSystem particle;			//爆発ParticleSystem
+
+	void Start () {
+		particle = GetComponent<ParticleSystem> ();
+	}
+	
+	void Update () {
+		//再生が終わったら
+		if(particle.isPlaying == false){
+			Destroy(gameObject);
+		}
+	}
+}
