41b62b985b7e29c043b4bce4a810b07bcfae852e
mekatama <tamatte@hotmail.com>
2018-11-20 19:47:09 +0900

    point表示用スクリプト追加

---
+++ UIstage_point.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIstage_point : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text pointText;				//Textコンポーネント取得用
+
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//score表示
+		pointText.text = gc.total_ItemNum.ToString("000000" + "p");
+	}
+}
