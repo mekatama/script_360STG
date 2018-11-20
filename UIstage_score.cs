e4e162bb94f12390018dc01632f208173b320fa9
mekatama <tamatte@hotmail.com>
2018-11-20 19:47:27 +0900

    score表示用スクリプト追加

---
+++ UIstage_score.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIstage_score : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text scoreText;				//Textコンポーネント取得用
+
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//score表示
+		scoreText.text = gc.total_Score.ToString("000000" + "p");
+	}
+}
