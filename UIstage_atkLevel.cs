e9968d65e86d67e72268279a478a8b70a32e9c43
mekatama <tamatte@hotmail.com>
2018-11-26 19:31:54 +0900

    ボタンにattackPowerUI表示

---
+++ UIstage_atkLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIstage_atkLevel : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text atkLevelText;			//Textコンポーネント取得用
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//powerLevel表示
+		atkLevelText.text = gc.attackPower.ToString("+"+ "0");
+	}
+}
