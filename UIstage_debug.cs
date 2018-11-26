40e02cab7d9cd2935e87f10e6be4de1f2ffc73e2
mekatama <tamatte@hotmail.com>
2018-11-26 19:32:20 +0900

    debug表示用のUI

---
+++ UIstage_debug.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIstage_debug : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text powerText;				//Textコンポーネント取得用
+	public Text levelText;				//Textコンポーネント取得用
+
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//debug表示
+		powerText.text = gc.attackPower.ToString("debug attackPower : "+ "00");
+		levelText.text = gc.shotLevel.ToString("debug shotLevel : " + "00");
+	}
+}
