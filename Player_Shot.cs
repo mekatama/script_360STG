ca4b36cd628d9e31d0625ab6e70ff514ec001d1b
mekatama <tamatte@hotmail.com>
2018-11-26 19:33:57 +0900

    連射間隔をGameControllerに移動

--- Player_Shot.cs
+++ Player_Shot.cs
@@ -6,7 +6,6 @@ public class Player_Shot : MonoBehaviour {
 	GameObject gameController;						//検索したオブジェクト入れる用
 	public GameObject bulletObject = null;			//弾プレハブ
 	public Transform bulletStartPosition = null;	//弾の発射位置を取得する
-	public float timeOut = 0.4f;					//弾の連射間隔
 	private float timeElapsed = 0.0f;				//弾の連射間隔カウント用
 
 	void Start () {
@@ -35,9 +34,11 @@ public class Player_Shot : MonoBehaviour {
 	}
 
 	void Shot(){
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
 		//弾の自動連射
 		timeElapsed += Time.deltaTime;
-        if(timeElapsed >= timeOut) {
+		if(timeElapsed >= gc.rappedTimeOut) {
 			//弾を生成する位置を指定する
 			Vector3 vecBulletPos	= bulletStartPosition.position;
 			//弾を生成する
