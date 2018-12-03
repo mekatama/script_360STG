ec4a1274698d07dd5d8dc623ec033b3cd21711a5
mekatama <tamatte@hotmail.com>
2018-12-03 18:45:04 +0900

    ui shildlevel表示

---
+++ UIstage_shildLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIstage_shildLevel : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text shildLevelText;			//Textコンポーネント取得用
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//powerLevel表示
+		if(gc.shildLevel == 4){
+			//level maxの時
+			shildLevelText.text = "MAX";
+		}else{
+			shildLevelText.text = gc.shildLevel.ToString("+"+ "0");
+		}
+	}
+}
