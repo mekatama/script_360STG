32c25fe80e00c2c8c933e2bddfef8716267d8beb
mekatama <tamatte@hotmail.com>
2018-12-03 18:45:37 +0900

    shildのサイズや表示のスクリプト

---
+++ Shild.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shild : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public int localShildLevel;			//shildLevel判別用の値
+	public float shildScale;			//shild size
+
+	void Start () {
+		this.transform.localScale = new Vector3(0, 0, 0);	//初期は非表示
+	}
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//shildLevelでsize分岐
+		if(gc.shildLevel == localShildLevel){
+			this.transform.localScale = new Vector3(shildScale, shildScale, shildScale);
+		}
+		//enemy接触時
+		if(gc.shildLevel == (localShildLevel -1)){
+			this.transform.localScale = new Vector3(0, 0, 0);	//非表示
+		}
+	}
+}
