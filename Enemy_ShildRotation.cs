fafae85d3fb685cd0c53cde5e5f999ad3f94422d
mekatama <tamatte@hotmail.com>
2018-12-20 19:51:09 +0900

    enemy周囲を回るshildの挙動

---
+++ Enemy_ShildRotation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_ShildRotation : MonoBehaviour {
+	public int enemyRotate;				//enemyの回転量
+
+	void Update () {
+		transform.Rotate(new Vector3(0,enemyRotate,0) * Time.deltaTime); 
+	}
+}
