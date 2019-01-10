80f7254723d1c6355e327a6c423ade2ff0ff5922
mekatama <tamatte@hotmail.com>
2019-01-10 19:26:33 +0900

    不要になりました

---
+++ Enemy_UI_Score.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy_UI_Score : MonoBehaviour {
+
+	void Start () {
+	}
+	
+	void Update () {
+		// 自身の向きをカメラに向ける
+        transform.rotation = Camera.main.transform.rotation;
+	}
+}
