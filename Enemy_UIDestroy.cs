38769f27b349c1a13087336ba7b06efd2d65e1c3
mekatama <tamatte@hotmail.com>
2019-01-10 19:27:04 +0900

    敵のスコア自動消滅

---
+++ Enemy_UIDestroy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_UIDestroy : MonoBehaviour {
+
+	void Start () {
+		Destroy(gameObject,1.0f);	//オブジェクトの削除
+	}
+}
