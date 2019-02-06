baea4014fd7593cecb2877bddf5eb41ef7485e38
mekatama <tamatte@hotmail.com>
2019-02-06 19:18:05 +0900

    画面遷移スクリプト

---
+++ ReturnController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnController : MonoBehaviour {
+
+	//戻るボタン用の制御関数
+	public void ButtonClicked_Title(){
+		SceneManager.LoadScene("title");	//シーンのロード
+	}
+}
---
+++ TitleController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour {
+
+	void Update(){
+		//backキー
+		if (Input.GetKeyUp(KeyCode.Escape)){
+			Application.Quit();	//アプリ終了
+		}
+	}
+
+	//セレクト画面用の制御関数
+	public void ButtonClicked_Start(){
+		SceneManager.LoadScene("stage1");	//シーンのロード
+	}
+
+	//遊び方ボタン用の制御関数
+	public void ButtonClicked_HowToPlay(){
+		SceneManager.LoadScene("HowToPlay");	//シーンのロード
+	}
+
+	//アプリ終了
+	public void ButtonClicked_Exit(){
+		Application.Quit();
+		Debug.Log("exit");	
+	}
+
+	//Debug用ハイスコアリセットボタン
+//	public void ButtonClicked_Reset(){
+//		PlayerPrefs.DeleteAll();
+//		Debug.Log("全データ削除しますた");	
+//	}
+}
