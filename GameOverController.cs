3af077aef6d680eedd0014e49df88e4f6a9c6eb4
mekatama <tamatte@hotmail.com>
2019-03-01 19:53:37 +0900

    newrecord表示処理

---
+++ GameOverController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverController : MonoBehaviour {
+	public Text newRecordText;	//Textコンポーネント取得用
+	private int isNewRecord;	//NewRecord flag用
+
+	void Start () {
+		//NewRecord flag用 0 = false
+		isNewRecord = PlayerPrefs.GetInt("NewRecord");
+		if(isNewRecord == 0){
+			newRecordText.enabled = false;		//UI非表示
+		}
+	}
+
+	void Update () {
+	}
+}
